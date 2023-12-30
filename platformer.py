b4433fae9833b5fbbea9b268f1de5197dd606ea2
JenDoll <joshberenguel@gmail.com>
2023-12-30 09:40:35 -0800

    first commit

---
+++ platformer.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our enemies
+    # update everything
+    screen.fill('black')
+    pygame.display.update()
+    clock.tick(60)
