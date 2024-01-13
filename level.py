fe8d61e46e402a780f10c88fdc6a8e1b48f0b11e
JenDoll <joshberenguel@gmail.com>
2024-01-13 14:56:04 -0800

    Add landing dust particle animation for player

--- level.py
+++ level.py
@@ -14,7 +14,7 @@ class Level:
 
         # dust
         self.dust_sprite = pygame.sprite.GroupSingle()
-
+        self.player_on_ground = False
 
     def create_jump_particles(self, pos):
         if self.player.sprite.facing == 'right':
@@ -25,13 +25,22 @@ class Level:
         jump_particle_sprite = ParticleEffect(pos,'jump')
         self.dust_sprite.add(jump_particle_sprite)
 
-
     def get_player_on_ground(self):
         if self.player.sprite.on_ground:
             self.player_on_ground = True
         else:
             self.player_on_ground = False
-    
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing == 'right':
+                offset = pygame.math.Vector2(10,15)
+            else:
+                offset = pygame.math.Vector2(-10,15)
+
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particle)
+
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
@@ -112,8 +121,8 @@ class Level:
         # player 
         self.player.update()
         self.horizontal_movement_collision()
-        self.vertical_movement_collision()
-
         self.get_player_on_ground()
+        self.vertical_movement_collision()
+        self.create_landing_dust()
         self.player.draw(self.display_surface)
         
